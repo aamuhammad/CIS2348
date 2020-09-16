5c08857876c9f2144ac24043fa252f144dcb28f5
aamuhammad <aamuhammad@uh.edu>
2020-09-16 14:35:11 -0500

    Updated 09/16/2020

--- zylab120/main.py
+++ zylab120/main.py
@@ -1,1 +1,11 @@
+print('Enter integer:')
+user_num1 = input()
+print('You entered:', user_num1)
+print(user_num1, 'squared is', user_num1 * user_num1)
+print('and', user_num1, 'cubed is', user_num1 * user_num1 * user_num1, '!!')
+print('Enter another integer:')
+user_num2 = input()
+print(user_num1, '+', user_num2, 'is', user_num1 + user_num2)
+print(user_num1, '*', user_num2, 'is', user_num1 * user_num2)
+
 
