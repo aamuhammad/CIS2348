4de0d049c6b532817ba2c1f7954106e718b5c6cd
aamuhammad <aamuhammad@uh.edu>
2020-10-07 14:14:29 -0500

    Updated 10/7/2020

--- Homework2/Lab_6_17.py
+++ Homework2/Lab_6_17.py
@@ -1,1 +1,18 @@
+simplepassword = input()
+password=''
+for x in simplepassword:
+    if(x == 'i'):
+        password += "!"
+    elif(x == 'a'):
+        password += "@"
+    elif(x == 'm'):
+        password += "M"
+    elif(x == 'B'):
+        password += "8"
+    elif(x == 'o'):
+        password += "."
+    else:
+        password += x
 
+password += "q*s"
+print(password)
