8ec2eb9718bf95cb3b210ca1fab89f7b6cf2993b
aamuhammad <aamuhammad@uh.edu>
2020-12-01 20:15:28 -0600

    Updated 10/7/2020

--- Homework 4/Lab_12_9.py
+++ Homework 4/Lab_12_9.py
@@ -1,1 +1,14 @@
+#Amaan Muhammad
+#PSID: 1607608
 
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    try:
+        age = int(parts[1]) + 1
+    except ValueError:
+        age = 0
+    print('{} {}'.format(name, age))
+
+    parts = input().split()
+    name = parts[0]
